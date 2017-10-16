d5125492828f419aa3009be5496fb8a98a16477e
Edd <pure1_16@uni.worc.ac.uk>
2017-10-16 15:00:13 +0100

    html added
    html added 16/10/17

---
+++ Worksheet 4.js
@@ -0,0 +1,14 @@
+//Variable Declaration
+
+window.localStorage.setItem("Calculator", "Red");
+window.localStorage.setItem("Phone", "Green");
+window.localStorage.setItem("Wallet", "Yellow");
+window.localStorage.setItem("Book", "Orange");
+window.localStorage.setItem("Stapler", "Brown");
+
+
+window.alert(window.localStorage.getItem("Calculator"));
+window.alert(window.localStorage.getItem("Phone"));
+window.alert(window.localStorage.getItem("Wallet"));
+window.alert(window.localStorage.getItem("Book"));
+window.alert(window.localStorage.getItem("Stapler"));
